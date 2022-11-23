2f24a3b9949ef87fffcad1fc3fe00f20eb7f21d7
updaun <updauney@daum.net>
2022-11-24 04:07:51 +0900

    [feat] Rooms Admin
    rooms admin panels with commonmodel

--- rooms/models.py
+++ rooms/models.py
@@ -11,6 +11,7 @@ class Room(CommonModel):
         PRIVATE_ROOM = ("private_room", "Private Room")
         SHARED_ROOM = ("shared_room", "Shared Room")
 
+    name = models.CharField(max_length=180, default="")
     country = models.CharField(max_length=50, default="대한민국")
     city = models.CharField(max_length=80, default="서울")
     price = models.PositiveIntegerField()
@@ -19,9 +20,12 @@ class Room(CommonModel):
     description = models.TextField()
     address = models.CharField(max_length=250)
     pet_friendly = models.BooleanField(default=True)
-    kind = models.CharField(max_length=20, choices=RoomKindChoices)
+    kind = models.CharField(max_length=20, choices=RoomKindChoices.choices)
     owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
     amenities = models.ManyToManyField("rooms.Amenity")
+
+    def __str__(self):
+        return self.name
 
 
 class Amenity(CommonModel):
@@ -29,4 +33,10 @@ class Amenity(CommonModel):
     """Amenity Model Definition"""
 
     name = models.CharField(max_length=150)
-    description = models.CharField(max_length=150, null=True)
+    description = models.CharField(max_length=150, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Amenities"
