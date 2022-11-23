8dc7949e2021e54521741138693265e4f0583a33
updaun <updauney@daum.net>
2022-11-23 21:09:18 +0900

    [feat] customize user admin panels

--- users/admin.py
+++ users/admin.py
@@ -5,4 +5,39 @@ from .models import User
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    pass
+    fieldsets = (
+        (
+            # "Profile",
+            None,
+            {
+                "fields": ("username", "password", "name", "email", "is_host"),
+                "classes": ("wide",),
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+                "classes": ("collapse",),
+            },
+        ),
+        (
+            "Important Dates",
+            {
+                "fields": (
+                    "last_login",
+                    "date_joined",
+                ),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+    # fields = ("email", "password", "name")
+
+    list_display = ("username", "email", "name", "is_host")
