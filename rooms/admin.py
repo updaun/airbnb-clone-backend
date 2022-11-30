c52998138ecf9a692b80cbcaceb8a926237fe8c0
updaun <updauney@daum.net>
2022-11-30 23:36:59 +0900

    [feat] word filter

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,8 +2,18 @@ from django.contrib import admin
 from .models import Room, Amenity
 
 
+@admin.action(description="Set all prices to zero")
+def reset_prices(model_admin, request, rooms):
+    for room in rooms.all():
+        room.price = 0
+        room.save()
+
+
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
+
+    actions = (reset_prices,)
+
     list_display = (
         "name",
         "price",
@@ -21,6 +31,13 @@ class RoomAdmin(admin.ModelAdmin):
         "amenities",
         "created_at",
         "updated_at",
+    )
+
+    search_fields = (
+        # "name",
+        # "^price", # __startswith
+        # "=price", # exact
+        "^owner__username",
     )
 
     def total_amenities(self, room):
