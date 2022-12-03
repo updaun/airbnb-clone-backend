e1c946c231d9c003c0c3bd17b85cee8ffc106d43
updaun <updauney@daum.net>
2022-12-04 02:56:27 +0900

    [feat] django render examples

---
+++ rooms/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path("", views.see_all_rooms),
+    path("<int:room_id>", views.see_one_room),
+]
--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Room
 
-# Create your views here.
+
+def see_all_rooms(request):
+    rooms = Room.objects.all()
+    return render(
+        request,
+        "rooms/all_rooms.html",
+        {"rooms": rooms, "title": "Hello! this title comes from django!"},
+    )
+
+
+def see_one_room(request, room_id):
+    try:
+        room = Room.objects.get(id=room_id)
+        return render(request, "rooms/room_detail.html", {"room": room})
+    except Room.DoesNotExist:
+        return render(request, "rooms/room_detail.html", {"not_found": True})
