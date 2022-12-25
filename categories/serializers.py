3c53b05192a8870497182f00796a7cc0bb278c6f
updaun <updauney@daum.net>
2022-12-25 21:39:08 +0900

    [feat] post method model create withour serializer

--- categories/serializers.py
+++ categories/serializers.py
@@ -4,6 +4,11 @@ from rest_framework import serializers
 class CategorySerializer(serializers.Serializer):
 
     pk = serializers.IntegerField()
-    name = serializers.CharField(required=True)
-    kind = serializers.CharField()
+    name = serializers.CharField(
+        required=True,
+        max_length=50,
+    )
+    kind = serializers.CharField(
+        max_length=15,
+    )
     created_at = serializers.DateTimeField()
