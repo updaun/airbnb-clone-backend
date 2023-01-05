2c3f1f1004754105926c430d21b13040754f1ed1
updaun <updauney@daum.net>
2023-01-05 23:27:58 +0900

    [feat] what serializer update() do

--- categories/views.py
+++ categories/views.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from django.core import serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
 from django.shortcuts import render
 from .models import Category
 from .serializers import CategorySerializer
@@ -32,8 +33,20 @@ def categories(request):
             return Response(serializer.errors)
 
 
-@api_view()
+@api_view(["GET", "PUT"])
 def category(request, pk):
-    category = Category.objects.get(pk=pk)
-    serializer = CategorySerializer(category)
-    return Response(serializer.data)
+    try:
+        category = Category.objects.get(pk=pk)
+    except Category.DoesNotExist:
+        raise NotFound
+
+    if request.method == "GET":
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = CategorySerializer(category, data=request.data, partial=True)
+        if serializer.is_valid():
+            updated_category = serializer.save()
+            return Response(CategorySerializer(updated_category).data)
+        else:
+            return Response(serializer.data)
