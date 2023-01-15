d7d9f389f10921cd1f626cfea412d651129befbb
updaun <updauney@daum.net>
2023-01-15 23:50:32 +0900

    230115 TIL
    에어비엔비 클론 코딩 - #10.10 APIView
    FBV to CBV with APIView

--- categories/views.py
+++ categories/views.py
@@ -7,6 +7,8 @@ from django.shortcuts import render
 from .models import Category
 from .serializers import CategorySerializer
 from rest_framework import status
+from rest_framework.views import APIView
+
 
 # restframework 없이 api를 만드는 방법
 # def categories(request):
@@ -19,13 +21,50 @@ from rest_framework import status
 #     )
 
 
-@api_view(["GET", "POST"])
-def categories(request):
-    if request.method == "GET":
+# @api_view(["GET", "POST"])
+# def categories(request):
+#     if request.method == "GET":
+#         all_categories = Category.objects.all()
+#         serializer = CategorySerializer(all_categories, many=True)
+#         return Response(serializer.data)
+#     elif request.method == "POST":
+#         serializer = CategorySerializer(data=request.data)
+#         if serializer.is_valid():
+#             new_category = serializer.save()
+#             return Response(CategorySerializer(new_category).data)
+#         else:
+#             return Response(serializer.errors)
+
+
+# @api_view(["GET", "PUT", "DELETE"])
+# def category(request, pk):
+#     try:
+#         category = Category.objects.get(pk=pk)
+#     except Category.DoesNotExist:
+#         raise NotFound
+
+#     if request.method == "GET":
+#         serializer = CategorySerializer(category)
+#         return Response(serializer.data)
+#     elif request.method == "PUT":
+#         serializer = CategorySerializer(category, data=request.data, partial=True)
+#         if serializer.is_valid():
+#             updated_category = serializer.save()
+#             return Response(CategorySerializer(updated_category).data)
+#         else:
+#             return Response(serializer.data)
+#     elif request.method == "DELETE":
+#         category.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class Categories(APIView):
+    def get(self, request):
         all_categories = Category.objects.all()
         serializer = CategorySerializer(all_categories, many=True)
         return Response(serializer.data)
-    elif request.method == "POST":
+
+    def post(self, request):
         serializer = CategorySerializer(data=request.data)
         if serializer.is_valid():
             new_category = serializer.save()
@@ -34,23 +73,27 @@ def categories(request):
             return Response(serializer.errors)
 
 
-@api_view(["GET", "PUT"])
-def category(request, pk):
-    try:
-        category = Category.objects.get(pk=pk)
-    except Category.DoesNotExist:
-        raise NotFound
+class CategoryDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            raise NotFound
 
-    if request.method == "GET":
-        serializer = CategorySerializer(category)
+    def get(self, request, pk):
+        serializer = CategorySerializer(self.get_object(pk))
         return Response(serializer.data)
-    elif request.method == "PUT":
-        serializer = CategorySerializer(category, data=request.data, partial=True)
+
+    def put(self, request, pk):
+        serializer = CategorySerializer(
+            self.get_object(pk), data=request.data, partial=True
+        )
         if serializer.is_valid():
             updated_category = serializer.save()
             return Response(CategorySerializer(updated_category).data)
         else:
             return Response(serializer.data)
-    elif request.method == "DELETE":
-        category.delete()
+
+    def delete(self, request, pk):
+        self.get_object(pk).delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
