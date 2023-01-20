52015d570dd489443aa7d88d0c07b9db7abf4882
updaun <updauney@daum.net>
2023-01-20 20:28:45 +0900

    [refactor] from serializer to model serializer

--- categories/serializers.py
+++ categories/serializers.py
@@ -2,24 +2,30 @@ from rest_framework import serializers
 from .models import Category
 
 
-class CategorySerializer(serializers.Serializer):
+# class CategorySerializer(serializers.Serializer):
 
-    pk = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(
-        required=True,
-        max_length=50,
-    )
-    kind = serializers.ChoiceField(
-        max_length=15,
-        choices=Category.CategoryKindChoices.choices,
-    )
-    created_at = serializers.DateTimeField(read_only=True)
+#     pk = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField(
+#         required=True,
+#         max_length=50,
+#     )
+#     kind = serializers.ChoiceField(
+#         max_length=15,
+#         choices=Category.CategoryKindChoices.choices,
+#     )
+#     created_at = serializers.DateTimeField(read_only=True)
 
-    def create(self, validated_data):
-        return Category.objects.create(**validated_data)
+#     def create(self, validated_data):
+#         return Category.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get("name", instance.name)
-        instance.kind = validated_data.get("kind", instance.kind)
-        instance.save()
-        return instance
+#     def update(self, instance, validated_data):
+#         instance.name = validated_data.get("name", instance.name)
+#         instance.kind = validated_data.get("kind", instance.kind)
+#         instance.save()
+#         return instance
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
