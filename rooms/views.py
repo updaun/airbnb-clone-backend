53811ef9d8fe012faa910d430542d25fadb2770c
updaun <updauney@daum.net>
2023-01-23 02:35:22 +0900

    [feat] create photo model 에어비엔비 클론 코딩 - #11.16 Upload Photo

--- rooms/views.py
+++ rooms/views.py
@@ -10,6 +10,7 @@ from .serializers import *
 from django.db import transaction
 from reviews.serializers import ReviewSerializer
 from django.conf import settings
+from medias.serializers import PhotoSerializer
 
 
 # def see_all_rooms(request):
@@ -213,5 +214,22 @@ class RoomAmenities(APIView):
 
 
 class RoomPhotos(APIView):
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise exceptions.NotFound
+
     def post(self, request, pk):
-        pass
+        room = self.get_object(pk)
+        if not request.user.is_authenticated:
+            raise exceptions.NotAuthenticated
+        if request.user != room.owner:
+            raise exceptions.PermissionDenied
+        serializer = PhotoSerializer(data=request.data)
+        if serializer.is_valid():
+            photo = serializer.save(room=room)
+            serializer = PhotoSerializer(photo)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
