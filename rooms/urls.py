f472df3ef1f6ae9c402984e6c6ed9fc881c927e5
updaun <updauney@daum.net>
2023-01-23 01:04:01 +0900

    [feat] review, amenity pagination 에어비엔비 클론 코딩 - #11.14 Pagination

--- rooms/urls.py
+++ rooms/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     # path("<int:room_id>", views.see_one_room),
     path("", views.Rooms.as_view()),
     path("<int:pk>", views.RoomDetail.as_view()),
+    path("<int:pk>/reviews", views.RoomReviews.as_view()),
+    path("<int:pk>/amenities", views.RoomAmenities.as_view()),
     path("amenities/", views.Amenities.as_view()),
     path("amenities/<int:pk>", views.AmenityDetail.as_view()),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -8,6 +8,7 @@ from rest_framework.response import Response
 from rest_framework import exceptions
 from .serializers import *
 from django.db import transaction
+from reviews.serializers import ReviewSerializer
 
 
 # def see_all_rooms(request):
@@ -166,3 +167,45 @@ class RoomDetail(APIView):
             raise exceptions.PermissionDenied
         room.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class RoomReviews(APIView):
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(id=pk)
+        except Room.DoesNotExist:
+            raise exceptions.NotFound
+
+    def get(self, request, pk):
+        try:
+            page = request.query_params.get("page", 1)
+            page = int(page)
+        except ValueError:
+            page = 1
+        page_size = 3
+        start = (page - 1) * page_size
+        end = start + page_size
+        room = self.get_object(pk)
+        serializer = ReviewSerializer(room.reviews.all()[start:end], many=True)
+        return Response(serializer.data)
+
+
+class RoomAmenities(APIView):
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(id=pk)
+        except Room.DoesNotExist:
+            raise exceptions.NotFound
+
+    def get(self, request, pk):
+        try:
+            page = request.query_params.get("page", 1)
+            page = int(page)
+        except ValueError:
+            page = 1
+        page_size = 3
+        start = (page - 1) * page_size
+        end = start + page_size
+        room = self.get_object(pk)
+        serializer = AmenitiySerializer(room.amenities.all()[start:end], many=True)
+        return Response(serializer.data)
