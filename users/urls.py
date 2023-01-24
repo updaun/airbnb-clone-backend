bbab057d448a07fbb0b9ffa426232363b6e48d01
updaun <updauney@daum.net>
2023-01-24 21:15:59 +0900

    [feat] user detail api, change password api 에어비엔비 클론 코딩 - #12.2 Change Password

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     path("", Users.as_view()),
     path("me", Me.as_view()),
+    path("change-password", ChangePassword.as_view()),
+    path("@<str:username>", PublicUser.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status, exceptions
 from rest_framework.permissions import IsAuthenticated
 from .serializers import PrivateUserSerializer
+from .models import User
 
 
 class Me(APIView):
@@ -37,3 +38,30 @@ class Users(APIView):
             serializer = PrivateUserSerializer(user)
             return Response(serializer.data)
         return Response(serializer.errors)
+
+
+class PublicUser(APIView):
+    def get(self, request, username):
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise exceptions.NotFound
+        serializer = PrivateUserSerializer(user)
+        return Response(serializer.data)
+
+
+class ChangePassword(APIView):
+
+    permission_classes = (IsAuthenticated,)
+
+    def put(self, request):
+        user = request.user
+        old_password = request.data.get("old_password")
+        new_password = request.data.get("new_password")
+        if not old_password or not new_password:
+            raise exceptions.ParseError
+        if user.check_password(old_password):
+            user.set_password(new_password)
+            user.save()
+            return Response(status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
