dddc00018abaf9ab8fbab5001a09ab4fd48828bc
updaun <updauney@daum.net>
2023-03-16 23:05:07 +0900

    #14.9 Permissions

---
+++ common/permissions.py
@@ -0,0 +1,10 @@
+from strawberry.type import Info
+import typing
+from strawberry.permission import BasePermission
+
+
+class OnlyLoggedIn(BasePermission):
+    message = "You need to be logged in for this!"
+
+    def has_permission(self, source: typing.Any, info: Info):
+        return info.context.user.is_authenticated
--- rooms/queries.py
+++ rooms/queries.py
@@ -1,5 +1,11 @@
 from . import models
 
+# from strawberry.types import Info
+# def get_all_rooms(info: Info):
+#     if info.context.request.user.is_authenticated:
+#         return models.Room.objects.all()
+#     return Exception("Not authenticated")
+
 
 def get_all_rooms():
     return models.Room.objects.all()
