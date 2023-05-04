04543480f0d41e10f5c53c3c411ed7a40cb72805
updaun <updauney@daum.net>
2023-05-04 16:23:38 +0900

    #20.1 Credentials

--- config/settings.py
+++ config/settings.py
@@ -172,4 +172,5 @@ REST_FRAMEWORK = {
     ]
 }
 
-CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000"]
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000", "http://localhost:3000"]
+CORS_ALLOW_CREDENTIALS = True
