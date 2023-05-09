426f4f1457fbffa2c7c4d42e4e340a810e93a31f
updaun <updauney@daum.net>
2023-05-09 22:55:15 +0900

    #20.7 Access Token

--- users/views.py
+++ users/views.py
@@ -7,6 +7,7 @@ from .serializers import PrivateUserSerializer
 from .models import User
 import jwt
 from django.conf import settings
+import requests
 
 
 class Me(APIView):
@@ -108,5 +109,17 @@ class JWTLogIn(APIView):
 class GithubLogIn(APIView):
     def post(self, request):
         code = request.data.get("code")
-        print(code)
+        access_token = requests.post(
+            f"https://github.com/login/oauth/access_token?code={code}&client_id=c60425707db819049b03&client_secret={settings.GH_SECRET}",
+            headers={"Accept": "application/json"},
+        )
+        access_token = access_token.json().get("access_token")
+        user_data = requests.get(
+            "https://api.github.com/user",
+            headers={
+                "Authorization": f"Bearer {access_token}",
+                "Accept": "application/json",
+            },
+        )
+        user_data = user_data.json()
         return Response()
